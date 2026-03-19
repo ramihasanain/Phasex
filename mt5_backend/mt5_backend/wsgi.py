4ede3edf956417e0bb4154f1495878fc96215e62
Rami Hasanain <rami.x.hasanain@gmail.com>
2026-03-19 03:28:07 +0300

    chore: delete a file.backend

--- mt5_backend/mt5_backend/wsgi.py
+++
@@ -1,8 +0,0 @@
-"""
-WSGI config for mt5_backend project.
-"""
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mt5_backend.settings')
-application = get_wsgi_application()
